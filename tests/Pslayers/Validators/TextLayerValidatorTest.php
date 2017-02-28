9e4e17546e9039e958e0d6784dd0a64fb7f7556a
Darryn Ten <darryn.ten@gmail.com>
2017-03-01 01:07:06 +0200

    Incorrect filename

--- tests/Pslayers/Validators/TextLayerValidatorTest.php
+++ tests/Pslayers/Validators/TextLayerValidatorTest.php
@@ -2,12 +2,46 @@
 
 namespace DarrynTen\Pslayers\Tests\Validators;
 
-use DarrynTen\Pslayers\Validators\ImageTypeValidator;
+use DarrynTen\Pslayers\Validators\TextLayerValidator;
+use DarrynTen\Pslayers\Exceptions\PslayersException;
 
-class ImageTypeValidatorTest extends \PHPUnit_Framework_TestCase
+class TextLayerValidatorTest extends \PHPUnit_Framework_TestCase
 {
     public function testValidation()
     {
-        // $this->assertTrue(Validation::
+        $this->assertTrue(TextLayerValidator::isValidTextAlignment(\Imagick::ALIGN_UNDEFINED));
+        $this->assertTrue(TextLayerValidator::isValidTextDecoration(\Imagick::DECORATION_UNDERLINE));
+        $this->assertTrue(TextLayerValidator::isValidFontStyle(\Imagick::STYLE_ITALIC));
+        $this->assertTrue(TextLayerValidator::isValidFontStretch(\Imagick::STRETCH_CONDENSED));
+    }
+
+    public function testFontStyleTypeError()
+    {
+        $this->expectException(\TypeError::class);
+        TextLayerValidator::isValidFontStyle('text');
+    }
+
+    public function testFontStyleException()
+    {
+        $this->expectException(PslayersException::class);
+        TextLayerValidator::isValidFontStyle(-1000);
+    }
+
+    public function testFontStretchException()
+    {
+        $this->expectException(PslayersException::class);
+        TextLayerValidator::isValidFontStretch(-0.1);
+    }
+
+    public function testTextAlignmentException()
+    {
+        $this->expectException(PslayersException::class);
+        TextLayerValidator::isValidTextAlignment(1000000000);
+    }
+
+    public function testTextDecorationException()
+    {
+        $this->expectException(PslayersException::class);
+        TextLayerValidator::isValidTextDecoration(1000000000);
     }
 }
